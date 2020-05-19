a2f77b369bd0be41b04eec34bcb15d8188d36d28
Alexandre <alexandre@thau.tech>
2020-05-19 19:52:56 -0300

    Exibe dados do banco na tabela.

--- src/app/aluno/aluno.component.ts
+++ src/app/aluno/aluno.component.ts
@@ -3,18 +3,7 @@ import { AlunoModelo } from "../modelos/aluno";
 import { AlunoService } from "../aluno.service";
 
 
-const ELEMENT_DATA: AlunoModelo[] = [
-  {id: "1", nome: 'Hydrogen', cpf: "1.0079", matricula: "H", dataNascimento: "2020"},
-  {id: "2", nome: 'Helium', cpf: "4.0026", matricula: 'He', dataNascimento: "2020"},
-  {id: "3", nome: 'Lithium', cpf: "6.941", matricula: 'Li', dataNascimento: "2020"},
-  {id: "4", nome: 'Beryllium', cpf: "9.0122", matricula: 'Be', dataNascimento: "2020"},
-  {id: "5", nome: 'Boron', cpf: "10.811", matricula: 'B', dataNascimento: "2020"},
-  {id: "6", nome: 'Carbon', cpf: "12.0107", matricula: 'C', dataNascimento: "2020"},
-  {id: "7", nome: 'Nitrogen', cpf: "14.0067", matricula: 'N', dataNascimento: "2020"},
-  {id: "8", nome: 'Oxygen', cpf: "15.9994", matricula: 'O', dataNascimento: "2020"},
-  {id: "9", nome: 'Fluorine', cpf: "18.9984", matricula: 'F', dataNascimento: "2020"},
-  {id: "10", nome: 'Neon', cpf: "20.1797", matricula: 'Ne', dataNascimento: "2020"},
-];
+const ELEMENT_DATA: AlunoModelo[] = [];
 
 @Component({
   selector: "app-aluno",
@@ -47,9 +36,9 @@ const ELEMENT_DATA: AlunoModelo[] = [
   </ng-container>
 
     <!-- matricula Column -->
-  <ng-container matColumnDef="matricula">
-    <th mat-header-cell *matHeaderCellDef> matricula </th>
-    <td mat-cell *matCellDef="let element"> {{element.matricula}} </td>
+  <ng-container matColumnDef="dataNascimento">
+    <th mat-header-cell *matHeaderCellDef> Data de nascimento </th>
+    <td mat-cell *matCellDef="let element"> {{element.dataNascimento}} </td>
   </ng-container>
 
   <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
@@ -74,6 +63,7 @@ export class AlunoComponent implements OnInit {
       .listaTodos()
       .then((entitidades: Array<AlunoModelo>) => {
         this.alunos = entitidades;
+        this.dataSource = entitidades;
       })
       .catch((erro: any) => {
         console.error(erro);
