08abd1c6f3dc4afe3fdde526fac454db6c1bde98
Alexandre <alexandre@thau.tech>
2020-05-26 20:23:57 -0300

    Adiciona aluno subject e carrega subject.

--- src/app/aluno/aluno.component.ts
+++ src/app/aluno/aluno.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { MatTableDataSource } from "@angular/material/table";
 import { MatPaginator } from "@angular/material/paginator";
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router } from "@angular/router";
+import { CollectionViewer, DataSource } from "@angular/cdk/collections";
+import { BehaviorSubject, Observable } from "rxjs";
 
 import { AlunoModelo } from "../modelos/aluno";
 import { AlunoService } from "../aluno.service";
@@ -12,41 +14,74 @@ const ELEMENT_DATA: AlunoModelo[] = [];
   selector: "app-aluno",
   template: `
     <div class="mat-elevation-z8">
-      <table mat-table [dataSource]="dataSource" >
+      <table mat-table [dataSource]="dataSource">
         <!-- Id Column -->
         <ng-container matColumnDef="id">
           <th mat-header-cell *matHeaderCellDef>id</th>
-          <td mat-cell *matCellDef="let element" (click)="navegaParaDetalhe(element)">{{ element.id }}</td>
+          <td
+            mat-cell
+            *matCellDef="let element"
+            (click)="navegaParaDetalhe(element)"
+          >
+            {{ element.id }}
+          </td>
         </ng-container>
 
         <!-- Nome Column -->
         <ng-container matColumnDef="nome">
           <th mat-header-cell *matHeaderCellDef>Nome</th>
-          <td mat-cell *matCellDef="let element" (click)="navegaParaDetalhe(element)">{{ element.nome }}</td>
+          <td
+            mat-cell
+            *matCellDef="let element"
+            (click)="navegaParaDetalhe(element)"
+          >
+            {{ element.nome }}
+          </td>
         </ng-container>
 
         <!-- Cpf Column -->
         <ng-container matColumnDef="cpf">
           <th mat-header-cell *matHeaderCellDef>Cpf</th>
-          <td mat-cell *matCellDef="let element" (click)="navegaParaDetalhe(element)">{{ element.cpf }}</td>
+          <td
+            mat-cell
+            *matCellDef="let element"
+            (click)="navegaParaDetalhe(element)"
+          >
+            {{ element.cpf }}
+          </td>
         </ng-container>
 
         <!-- Matricula Column -->
         <ng-container matColumnDef="matricula">
           <th mat-header-cell *matHeaderCellDef>Matricula</th>
-          <td mat-cell *matCellDef="let element" (click)="navegaParaDetalhe(element)">{{ element.matricula }}</td>
+          <td
+            mat-cell
+            *matCellDef="let element"
+            (click)="navegaParaDetalhe(element)"
+          >
+            {{ element.matricula }}
+          </td>
         </ng-container>
 
         <!-- Data de Nascimento Column -->
         <ng-container matColumnDef="dataNascimento">
           <th mat-header-cell *matHeaderCellDef>Data de nascimento</th>
-          <td mat-cell *matCellDef="let element" (click)="navegaParaDetalhe(element)">{{ element.dataNascimento }}</td>
+          <td
+            mat-cell
+            *matCellDef="let element"
+            (click)="navegaParaDetalhe(element)"
+          >
+            {{ element.dataNascimento }}
+          </td>
         </ng-container>
 
         <tr mat-header-row *matHeaderRowDef="colunas"></tr>
         <tr mat-row *matRowDef="let row; columns: colunas"></tr>
       </table>
-      <mat-paginator [pageSizeOptions]="[5, 10, 20]" showFirstLastButtons></mat-paginator>
+      <mat-paginator
+        [pageSizeOptions]="[5, 10, 20]"
+        showFirstLastButtons
+      ></mat-paginator>
     </div>
   `,
   styles: [``],
@@ -58,12 +93,35 @@ export class AlunoComponent implements OnInit {
 
   dataSource = new MatTableDataSource<AlunoModelo>(ELEMENT_DATA);
 
+  private alunosSubject = new BehaviorSubject<AlunoModelo[]>([]);
+
+  private carregaSubject = new BehaviorSubject<boolean>(false);
+
+  public loading$ = this.carregaSubject.asObservable();
+
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
   constructor(private alunoService: AlunoService, private router: Router) {}
 
   ngOnInit(): void {
     this.dataSource.paginator = this.paginator;
+    this.carregaAlunos();
+  }
+
+  navegaParaDetalhe(entrada: AlunoModelo) {
+    this.router.navigate(["detalhe/" + entrada.id]);
+  }
+
+  conecta(collectionViewer: CollectionViewer): Observable<AlunoModelo[]> {
+    return this.alunosSubject.asObservable();
+  }
+
+  desconecta(collectionViewer: CollectionViewer): void {
+    this.alunosSubject.complete();
+    this.carregaSubject.complete();
+  }
+
+  carregaAlunos() {
     this.alunoService
       .listaTodos()
       .then((entitidades: Array<AlunoModelo>) => {
@@ -73,9 +131,5 @@ export class AlunoComponent implements OnInit {
       .catch((erro: any) => {
         console.error(erro);
       });
-  }
-
-  navegaParaDetalhe(entrada: AlunoModelo){
-    this.router.navigate(['detalhe/' + entrada.id])
   }
 }
